c3d6eb9bb4ce52843245fc7cd564a8b71cc400f2
craig91 <craigdejean1@gmail.com>
2016-09-12 23:43:35 -0400

    One Folder Update

---
+++ React_1/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// class App extends React.Component {
+//   render(){
+//     return <div>Hello</div>
+//   }
+// }
+
+// class App extends React.Component{
+//   render(){
+//     return React.createElement('h1', null, 'Waddup Craig')
+//   }
+// }
+
+const App = () => <h1>Hello Eggheads</h1>
+
+export default App
